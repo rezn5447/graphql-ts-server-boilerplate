34342f4acfee2467246e3e90e77da02ef3c71f3c
Resdan Logan <rezn5447@gmail.com>
2018-04-28 13:23:43 -0700

    yarn package update

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,11 @@ createConnection().then(async connection => {
     user.age = 25;
     await connection.manager.save(user);
     console.log("Saved a new user with id: " + user.id);
-    
+
     console.log("Loading users from the database...");
     const users = await connection.manager.find(User);
     console.log("Loaded users: ", users);
-     
+
     console.log("Here you can setup and run express/koa/any other framework.");
-    
+
 }).catch(error => console.log(error));
